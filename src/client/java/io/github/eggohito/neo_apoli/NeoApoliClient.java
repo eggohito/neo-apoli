84dcbd70aac5910ae9e5dd54d9afb2e57f933d55
eggohito <66972816+eggohito@users.noreply.github.com>
2025-02-11 19:26:59 +0800

    Initial setup

--- src/client/java/io/github/eggohito/neo_apoli/NeoApoliClient.java
+++ src/client/java/io/github/eggohito/neo_apoli/NeoApoliClient.java
@@ -1,10 +1,12 @@
-package com.example;
+package io.github.eggohito.neo_apoli;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class NeoApoliClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
+
 }
---
+++ src/main/java/io/github/eggohito/neo_apoli/NeoApoli.java
@@ -0,0 +1,18 @@
+package io.github.eggohito.neo_apoli;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeoApoli implements ModInitializer {
+
+	public static final String MOD_NAMESPACE = "neo-apoli";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAMESPACE);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+
+}
