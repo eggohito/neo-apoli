1a4b588ce9f7d3d1087871ccb915cd13391696d8
eggohito <66972816+eggohito@users.noreply.github.com>
2025-05-16 12:47:09 +0800

    Fixed registrant method of block action types not being called

--- src/main/java/io/github/eggohito/neo_apoli/action/type/ActionTypes.java
+++ src/main/java/io/github/eggohito/neo_apoli/action/type/ActionTypes.java
@@ -1,10 +1,12 @@
 package io.github.eggohito.neo_apoli.action.type;
 
+import io.github.eggohito.neo_apoli.action.type.block.BlockActionTypes;
 import io.github.eggohito.neo_apoli.action.type.entity.EntityActionTypes;
 
 public final class ActionTypes {
 
 	public static void registerAll() {
+		BlockActionTypes.registerAll();
 		EntityActionTypes.registerAll();
 	}
 
