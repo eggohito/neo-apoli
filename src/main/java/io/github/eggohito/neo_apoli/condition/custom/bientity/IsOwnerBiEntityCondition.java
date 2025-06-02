f1dd232da6a9b330cf967e5cc3b23612d0b88595
eggohito <66972816+eggohito@users.noreply.github.com>
2025-06-02 18:37:46 +0800

    Added `is_owner` bi-entity condition type Checks if the 'actor' entity is the owner of the 'target' entity

---
+++ src/main/java/io/github/eggohito/neo_apoli/condition/custom/bientity/IsOwnerBiEntityCondition.java
@@ -0,0 +1,38 @@
+package io.github.eggohito.neo_apoli.condition.custom.bientity;
+
+import com.mojang.serialization.MapCodec;
+import io.github.eggohito.neo_apoli.condition.BiEntityCondition;
+import io.github.eggohito.neo_apoli.condition.type.bientity.BiEntityConditionType;
+import io.github.eggohito.neo_apoli.condition.type.bientity.BiEntityConditionTypes;
+import io.github.eggohito.neo_apoli.util.context.Context;
+import io.github.eggohito.neo_apoli.util.context.ContextParameters;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.Ownable;
+import net.minecraft.entity.Tameable;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+
+import java.util.Objects;
+
+public record IsOwnerBiEntityCondition() implements BiEntityCondition {
+
+	public static final MapCodec<IsOwnerBiEntityCondition> CODEC = MapCodec.unit(IsOwnerBiEntityCondition::new);
+	public static final PacketCodec<RegistryByteBuf, IsOwnerBiEntityCondition> PACKET_CODEC = PacketCodec.unit(new IsOwnerBiEntityCondition());
+
+	@Override
+	public BiEntityConditionType<?> getType() {
+		return BiEntityConditionTypes.IS_OWNER;
+	}
+
+	@Override
+	public boolean test(Context context) {
+
+		Entity actor = context.required(ContextParameters.ACTOR);
+		Entity target = context.required(ContextParameters.TARGET);
+
+		return (target instanceof Tameable tameable && Objects.equals(actor, tameable.getOwner()))
+			|| (target instanceof Ownable ownable && Objects.equals(actor, ownable.getOwner()));
+
+	}
+
+}
--- src/main/java/io/github/eggohito/neo_apoli/condition/type/bientity/BiEntityConditionTypes.java
+++ src/main/java/io/github/eggohito/neo_apoli/condition/type/bientity/BiEntityConditionTypes.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.MapCodec;
 import io.github.eggohito.neo_apoli.NeoApoli;
 import io.github.eggohito.neo_apoli.condition.BiEntityCondition;
 import io.github.eggohito.neo_apoli.condition.custom.bientity.EqualsBiEntityCondition;
+import io.github.eggohito.neo_apoli.condition.custom.bientity.IsOwnerBiEntityCondition;
 import io.github.eggohito.neo_apoli.condition.custom.bientity.SwapEntityContextBiEntityCondition;
 import io.github.eggohito.neo_apoli.condition.custom.bientity.TestEntityConditionBiEntityCondition;
 import io.github.eggohito.neo_apoli.condition.meta.bientity.*;
@@ -33,6 +34,7 @@ public final class BiEntityConditionTypes {
 	public static final BiEntityConditionType<ReferenceBiEntityCondition> REFERENCE = registerInternal("reference", ReferenceBiEntityCondition.CODEC, ReferenceBiEntityCondition.PACKET_CODEC);
 
 	public static final BiEntityConditionType<EqualsBiEntityCondition> EQUALS = registerInternal("equals", EqualsBiEntityCondition.CODEC, EqualsBiEntityCondition.PACKET_CODEC);
+	public static final BiEntityConditionType<IsOwnerBiEntityCondition> IS_OWNER = registerInternal("is_owner", IsOwnerBiEntityCondition.CODEC, IsOwnerBiEntityCondition.PACKET_CODEC);
 	public static final BiEntityConditionType<SwapEntityContextBiEntityCondition> SWAP_ENTITY_CONTEXT = registerInternal("swap_entity_context", SwapEntityContextBiEntityCondition.CODEC, SwapEntityContextBiEntityCondition.PACKET_CODEC);
 	public static final BiEntityConditionType<TestEntityConditionBiEntityCondition> TEST_ENTITY_CONDITION = registerInternal("test_entity_condition", TestEntityConditionBiEntityCondition.CODEC, TestEntityConditionBiEntityCondition.PACKET_CODEC);
 
