5ae7e29ec0351c6dd2ee346cd799bdac25515704
eggohito <66972816+eggohito@users.noreply.github.com>
2025-06-06 17:38:57 +0800

    Added `entity_type` entity condition type

---
+++ src/main/java/io/github/eggohito/neo_apoli/condition/custom/entity/EntityTypeEntityCondition.java
@@ -0,0 +1,39 @@
+package io.github.eggohito.neo_apoli.condition.custom.entity;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.eggohito.neo_apoli.condition.EntityCondition;
+import io.github.eggohito.neo_apoli.condition.type.entity.EntityConditionType;
+import io.github.eggohito.neo_apoli.condition.type.entity.EntityConditionTypes;
+import io.github.eggohito.neo_apoli.util.context.Context;
+import io.github.eggohito.neo_apoli.util.context.ContextParameters;
+import net.minecraft.entity.EntityType;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.registry.RegistryKeys;
+
+import java.util.Objects;
+
+public record EntityTypeEntityCondition(EntityType<?> entityType) implements EntityCondition {
+
+	public static final MapCodec<EntityTypeEntityCondition> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+		EntityType.CODEC.fieldOf("entity_type").forGetter(EntityTypeEntityCondition::entityType)
+	).apply(instance, EntityTypeEntityCondition::new));
+
+	public static final PacketCodec<RegistryByteBuf, EntityTypeEntityCondition> PACKET_CODEC = PacketCodec.tuple(
+		PacketCodecs.registryValue(RegistryKeys.ENTITY_TYPE), EntityTypeEntityCondition::entityType,
+		EntityTypeEntityCondition::new
+	);
+
+	@Override
+	public EntityConditionType<?> getType() {
+		return EntityConditionTypes.ENTITY_TYPE;
+	}
+
+	@Override
+	public boolean test(Context context) {
+		return Objects.equals(context.required(ContextParameters.THIS_ENTITY).getType(), this.entityType());
+	}
+
+}
--- src/main/java/io/github/eggohito/neo_apoli/condition/type/entity/EntityConditionTypes.java
+++ src/main/java/io/github/eggohito/neo_apoli/condition/type/entity/EntityConditionTypes.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import io.github.eggohito.neo_apoli.NeoApoli;
 import io.github.eggohito.neo_apoli.condition.EntityCondition;
+import io.github.eggohito.neo_apoli.condition.custom.entity.EntityTypeEntityCondition;
 import io.github.eggohito.neo_apoli.condition.custom.entity.IsSneakingEntityCondition;
 import io.github.eggohito.neo_apoli.condition.custom.entity.IsSprintingEntityCondition;
 import io.github.eggohito.neo_apoli.condition.custom.entity.OnBlockEntityCondition;
@@ -32,6 +33,7 @@ public final class EntityConditionTypes {
 	public static final EntityConditionType<InvertedEntityCondition> INVERTED = registerInternal("inverted", InvertedEntityCondition.CODEC, InvertedEntityCondition.PACKET_CODEC);
 	public static final EntityConditionType<ReferenceEntityCondition> REFERENCE = registerInternal("reference", ReferenceEntityCondition.CODEC, ReferenceEntityCondition.PACKET_CODEC);
 
+	public static final EntityConditionType<EntityTypeEntityCondition> ENTITY_TYPE = registerInternal("entity_type", EntityTypeEntityCondition.CODEC, EntityTypeEntityCondition.PACKET_CODEC);
 	public static final EntityConditionType<IsSneakingEntityCondition> IS_SNEAKING = registerInternal("is_sneaking", IsSneakingEntityCondition.CODEC, IsSneakingEntityCondition.PACKET_CODEC);
 	public static final EntityConditionType<IsSprintingEntityCondition> IS_SPRINTING = registerInternal("is_sprinting", IsSprintingEntityCondition.CODEC, IsSprintingEntityCondition.PACKET_CODEC);
 	public static final EntityConditionType<OnBlockEntityCondition> ON_BLOCK = registerInternal("on_block", OnBlockEntityCondition.CODEC, OnBlockEntityCondition.PACKET_CODEC);
