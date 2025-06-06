beb0bf909035c30b5e994210f69439d8e7e36a06
eggohito <66972816+eggohito@users.noreply.github.com>
2025-06-06 20:08:04 +0800

    Added a public generic context type builder in `PowerContextTypes`

--- src/main/java/io/github/eggohito/neo_apoli/power/context/PowerContextTypes.java
+++ src/main/java/io/github/eggohito/neo_apoli/power/context/PowerContextTypes.java
@@ -5,14 +5,17 @@ import net.minecraft.util.context.ContextType;
 
 public class PowerContextTypes {
 
-	public static final ContextType GENERIC = new ContextType.Builder()
+	public static final ContextType.Builder BUILDER = new ContextType.Builder()
 		.require(ContextParameters.POSITION)
 		.require(ContextParameters.THIS_ENTITY)
+		.allow(ContextParameters.ACTOR)
+		.allow(ContextParameters.TARGET)
+		.allow(ContextParameters.ITEM_STACK);
+
+	public static final ContextType GENERIC = BUILDER
 		.build();
 
-	public static final ContextType BLOCK = new ContextType.Builder()
-		.require(ContextParameters.POSITION)
-		.require(ContextParameters.THIS_ENTITY)
+	public static final ContextType BLOCK = BUILDER
 		.require(ContextParameters.BLOCK_STATE)
 		.allow(ContextParameters.BLOCK_ENTITY)
 		.allow(ContextParameters.DIRECTION)
