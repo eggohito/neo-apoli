06316c72700f29609c2e90ebd0e8e313392c0c01
eggohito <66972816+eggohito@users.noreply.github.com>
2025-09-23 04:07:37 +0800

    Fixed damage condition types not being initially registered at the same time as other condition types

--- src/main/java/io/github/eggohito/neo_apoli/condition/type/ConditionTypes.java
+++ src/main/java/io/github/eggohito/neo_apoli/condition/type/ConditionTypes.java
@@ -2,6 +2,7 @@ package io.github.eggohito.neo_apoli.condition.type;
 
 import io.github.eggohito.neo_apoli.condition.type.bientity.BiEntityConditionTypes;
 import io.github.eggohito.neo_apoli.condition.type.block.BlockConditionTypes;
+import io.github.eggohito.neo_apoli.condition.type.damage.DamageConditionTypes;
 import io.github.eggohito.neo_apoli.condition.type.entity.EntityConditionTypes;
 import io.github.eggohito.neo_apoli.condition.type.item.ItemConditionTypes;
 
@@ -10,6 +11,7 @@ public final class ConditionTypes {
 	public static void registerAll() {
 		BiEntityConditionTypes.registerAll();
 		BlockConditionTypes.registerAll();
+		DamageConditionTypes.registerAll();
 		EntityConditionTypes.registerAll();
 		ItemConditionTypes.registerAll();
 	}
