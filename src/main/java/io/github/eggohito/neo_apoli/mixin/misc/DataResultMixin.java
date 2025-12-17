b330e175a14f3ebaf245682a4b380de8a20cbbb7
eggohito <66972816+eggohito@users.noreply.github.com>
2025-12-18 01:16:48 +0800

    Tweaked how errors are appended in either codecs and data results The errors are now appended with a newline prefix so each error will be placed in a new line instead of being separated with a semicolon

---
+++ src/main/java/io/github/eggohito/neo_apoli/mixin/misc/DataResultMixin.java
@@ -0,0 +1,20 @@
+package io.github.eggohito.neo_apoli.mixin.misc;
+
+import com.mojang.serialization.DataResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(DataResult.class)
+public interface DataResultMixin {
+
+	/**
+	 * @author	eggohito
+	 * @reason	To append the error messages in a readable way, although it may take up more space vertically.
+	 */
+	@Overwrite(remap = false)
+	static String appendMessages(String first, String second) {
+		return	"\n\t - " + first +
+				"\n\t - " + second;
+	}
+
+}
