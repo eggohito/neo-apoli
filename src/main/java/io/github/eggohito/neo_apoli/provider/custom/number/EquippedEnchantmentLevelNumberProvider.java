2336ed67da4a2d321544c2fe94605238bcc40d7e
eggohito <66972816+eggohito@users.noreply.github.com>
2025-12-24 22:36:37 +0800

    Added `equipped_enchantment_level` number provider type A number provider that provides the level of the specified enchantment from the specified slot group. Has the following fields: - `enchantment`; the ID of the enchantment to get the level of - `slot_group`; the name of the slot group. Accepts any of "any", "mainhand", "offhand", "hand", "feet", "legs", "chest", "head", "armor", "body", or "saddle." - `calculation`; determines whether all the levels will be summarized ("sum") or whether the highest of all levels is selected ("max") - `entity`; the entity to check the equipped items with the specified enchantment from

---
+++ src/main/java/io/github/eggohito/neo_apoli/provider/custom/number/EquippedEnchantmentLevelNumberProvider.java
@@ -0,0 +1,133 @@
+package io.github.eggohito.neo_apoli.provider.custom.number;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.eggohito.neo_apoli.codec.NeoApoliCodecs;
+import io.github.eggohito.neo_apoli.codec.NeoApoliStreamCodecs;
+import io.github.eggohito.neo_apoli.provider.type.number.NumberProviderType;
+import io.github.eggohito.neo_apoli.provider.type.number.NumberProviderTypes;
+import io.github.eggohito.neo_apoli.util.CodecUtil;
+import io.github.eggohito.neo_apoli.util.StreamCodecUtil;
+import io.github.eggohito.neo_apoli.util.context.Context;
+import io.github.eggohito.neo_apoli.util.context.parameter.TypedContextKey;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.core.Holder;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.util.context.ContextKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Set;
+
+public record EquippedEnchantmentLevelNumberProvider(Holder<Enchantment> enchantment, EquipmentSlotGroup slotGroup, Calculation calculation, TypedContextKey<Entity> entity) implements NumberProvider {
+
+	public static final MapCodec<EquippedEnchantmentLevelNumberProvider> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+		Enchantment.CODEC.fieldOf("enchantment").forGetter(EquippedEnchantmentLevelNumberProvider::enchantment),
+		EquipmentSlotGroup.CODEC.optionalFieldOf("slot_group", EquipmentSlotGroup.ANY).forGetter(EquippedEnchantmentLevelNumberProvider::slotGroup),
+		Calculation.CODEC.optionalFieldOf("calculation", Calculation.MAX).forGetter(EquippedEnchantmentLevelNumberProvider::calculation),
+		NeoApoliCodecs.ENTITY_CONTEXT_KEY.fieldOf("entity").forGetter(EquippedEnchantmentLevelNumberProvider::entity)
+	).apply(instance, EquippedEnchantmentLevelNumberProvider::new));
+
+	public static final StreamCodec<RegistryFriendlyByteBuf, EquippedEnchantmentLevelNumberProvider> STREAM_CODEC = StreamCodec.composite(
+		Enchantment.STREAM_CODEC, EquippedEnchantmentLevelNumberProvider::enchantment,
+		EquipmentSlotGroup.STREAM_CODEC, EquippedEnchantmentLevelNumberProvider::slotGroup,
+		Calculation.STREAM_CODEC, EquippedEnchantmentLevelNumberProvider::calculation,
+		NeoApoliStreamCodecs.ENTITY_CONTEXT_KEY, EquippedEnchantmentLevelNumberProvider::entity,
+		EquippedEnchantmentLevelNumberProvider::new
+	);
+
+	@Override
+	public NumberProviderType<?> getType() {
+		return NumberProviderTypes.EQUIPPED_ENCHANTMENT_LEVEL;
+	}
+
+	@Override
+	public @NotNull Number next(Context context) {
+
+		switch (context.nullable(entity())) {
+			case LivingEntity livingEntity -> {
+				return calculation().getValue(livingEntity, enchantment(), slotGroup());
+			}
+			case null ->
+				context.forChild(".entity").report("Couldn't get enchantment levels of equipped items from non-existent entity!");
+			default ->
+				context.forChild(".entity").report("Couldn't get enchantment levels of equipped items from non-living entity!");
+		}
+
+		return 0;
+
+	}
+
+	@Override
+	public Set<ContextKey<?>> getRequiredParameters() {
+		return Set.of(entity());
+	}
+
+	public enum Calculation {
+
+		SUM {
+
+			@Override
+			public int getValue(LivingEntity wearer, Holder<Enchantment> enchantment, EquipmentSlotGroup slotGroup) {
+
+				Map<EquipmentSlot, ItemStack> slotItems = enchantment.value().getSlotItems(wearer);
+				int levels = 0;
+
+				for (var entry : slotItems.entrySet()) {
+
+					EquipmentSlot slot = entry.getKey();
+					ItemStack stack = entry.getValue();
+
+					if (slotGroup.test(slot)) {
+						levels += stack.getEnchantments().getLevel(enchantment);
+					}
+
+				}
+
+				return levels;
+
+			}
+
+		},
+
+		MAX {
+
+			@Override
+			public int getValue(LivingEntity wearer, Holder<Enchantment> enchantment, EquipmentSlotGroup slotGroup) {
+
+				Map<EquipmentSlot, ItemStack> slotItems = enchantment.value().getSlotItems(wearer);
+				int levels = 0;
+
+				for (var entry : slotItems.entrySet()) {
+
+					EquipmentSlot slot = entry.getKey();
+					ItemStack stack = entry.getValue();
+
+					if (slotGroup.test(slot)) {
+						levels = Math.max(levels, stack.getEnchantments().getLevel(enchantment));
+					}
+
+				}
+
+				return levels;
+
+			}
+
+		};
+
+		public static final Codec<Calculation> CODEC = CodecUtil.enumType(Calculation.class);
+		public static final StreamCodec<ByteBuf, Calculation> STREAM_CODEC = StreamCodecUtil.enumType(Calculation.class);
+
+		public abstract int getValue(LivingEntity wearer, Holder<Enchantment> enchantmentHolder, EquipmentSlotGroup slotGroup);
+
+	}
+
+}
