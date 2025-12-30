49557dfd538ddb2443237f563850f677b64973e6
eggohito <66972816+eggohito@users.noreply.github.com>
2025-12-30 20:47:39 +0800

    Added `box/size` number provider type A number provider that provides the size of the box by getting the average of the edges of the specified box

---
+++ src/main/java/io/github/eggohito/neo_apoli/provider/custom/number/BoxSizeNumberProvider.java
@@ -0,0 +1,52 @@
+package io.github.eggohito.neo_apoli.provider.custom.number;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.eggohito.neo_apoli.provider.custom.box.BoxProvider;
+import io.github.eggohito.neo_apoli.provider.type.number.NumberProviderType;
+import io.github.eggohito.neo_apoli.provider.type.number.NumberProviderTypes;
+import io.github.eggohito.neo_apoli.util.context.Context;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.NotNull;
+
+public record BoxSizeNumberProvider(BoxProvider box) implements NumberProvider {
+
+	public static final MapCodec<BoxSizeNumberProvider> CODEC = RecordCodecBuilder.mapCodec(instance -> instance
+		.group(BoxProvider.CODEC.fieldOf("box").forGetter(BoxSizeNumberProvider::box))
+		.apply(instance, BoxSizeNumberProvider::new));
+
+	public static final StreamCodec<RegistryFriendlyByteBuf, BoxSizeNumberProvider> STREAM_CODEC = StreamCodec.composite(
+		BoxProvider.STREAM_CODEC, BoxSizeNumberProvider::box,
+		BoxSizeNumberProvider::new
+	);
+
+	@Override
+	public NumberProviderType<?> getType() {
+		return NumberProviderTypes.BOX_SIZE;
+	}
+
+	@Override
+	public @NotNull Number next(Context context) {
+
+		Context boxContext = context.forChild(".box");
+		AABB box = box().next(boxContext);
+
+		if (boxContext.hasErrors()) {
+			return 0.0D;
+		}
+
+		else {
+			return box.getSize();
+		}
+
+	}
+
+	@Override
+	public void validate(Context.Validator validator) {
+		NumberProvider.super.validate(validator);
+		box().validate(validator.forChild(".box"));
+	}
+
+}
