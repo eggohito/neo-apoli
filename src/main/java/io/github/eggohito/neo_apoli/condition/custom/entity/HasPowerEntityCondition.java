76cfb742d6153b956a45f51829bbe9e5c355ff9e
eggohito <66972816+eggohito@users.noreply.github.com>
2025-12-30 20:38:48 +0800

    Added `has_power` entity condition type An entity condition type that checks if the entity has the specified power, optionally from the specified source. Supports the following fields: - `power`; the ID of the power to check for from the entity - `source`; the optional ID of the source the power is from

---
+++ src/main/java/io/github/eggohito/neo_apoli/condition/custom/entity/HasPowerEntityCondition.java
@@ -0,0 +1,68 @@
+package io.github.eggohito.neo_apoli.condition.custom.entity;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.eggohito.neo_apoli.component.NeoApoliEntityComponents;
+import io.github.eggohito.neo_apoli.component.entity.PowersComponent;
+import io.github.eggohito.neo_apoli.condition.type.entity.EntityConditionType;
+import io.github.eggohito.neo_apoli.condition.type.entity.EntityConditionTypes;
+import io.github.eggohito.neo_apoli.power.PowerEntry;
+import io.github.eggohito.neo_apoli.power.PowerManager;
+import io.github.eggohito.neo_apoli.util.DynamicResourceLocation;
+import io.github.eggohito.neo_apoli.util.PowerReference;
+import io.github.eggohito.neo_apoli.util.context.Context;
+import io.github.eggohito.neo_apoli.util.context.NeoApoliContextKeys;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Optional;
+
+public record HasPowerEntityCondition(PowerReference power, Optional<ResourceLocation> source) implements EntityCondition {
+
+	public static final MapCodec<HasPowerEntityCondition> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+		PowerReference.CODEC.fieldOf("power").forGetter(HasPowerEntityCondition::power),
+		DynamicResourceLocation.CODEC.optionalFieldOf("source").forGetter(HasPowerEntityCondition::source)
+	).apply(instance, HasPowerEntityCondition::new));
+
+	public static final StreamCodec<RegistryFriendlyByteBuf, HasPowerEntityCondition> STREAM_CODEC = StreamCodec.composite(
+		PowerReference.STREAM_CODEC, HasPowerEntityCondition::power,
+		ByteBufCodecs.optional(ResourceLocation.STREAM_CODEC), HasPowerEntityCondition::source,
+		HasPowerEntityCondition::new
+	);
+
+	@Override
+	public EntityConditionType<?> getType() {
+		return EntityConditionTypes.HAS_POWER;
+	}
+
+	@Override
+	public boolean test(Context context) {
+
+		PowerEntry<?> entry = PowerManager.getEntryAsResult(this.power())
+			.result()
+			.orElse(null);
+		PowersComponent powersComponent = context.optional(NeoApoliContextKeys.THIS_ENTITY)
+			.flatMap(NeoApoliEntityComponents.POWERS::maybeGet)
+			.orElse(null);
+
+		return entry != null
+			&& powersComponent != null
+			&& this.testInternal(powersComponent, entry);
+
+	}
+
+	@Override
+	public void validate(Context.Validator validator) {
+		EntityCondition.super.validate(validator);
+		PowerManager.getEntryAsResult(this.power()).ifError(error -> validator.forChild(".power").report(error.message()));
+	}
+
+	private boolean testInternal(PowersComponent powersComponent, PowerEntry<?> entry) {
+		return this.source()
+			.map(innerSource -> powersComponent.hasInstance(entry, innerSource))
+			.orElseGet(() -> powersComponent.hasInstance(entry));
+	}
+
+}
