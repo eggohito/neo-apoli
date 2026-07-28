e614f2941cd10d7b4459949bd48a3fcca2b4f8b3
eggohito <66972816+eggohito@users.noreply.github.com>
2026-07-29 01:49:13 +0800

    Changed how errors are logged when loading a service

--- src/main/java/io/github/eggohito/neo_apoli/util/services/Services.java
+++ src/main/java/io/github/eggohito/neo_apoli/util/services/Services.java
@@ -1,17 +1,18 @@
 package io.github.eggohito.neo_apoli.util.services;
 
+import io.github.eggohito.neo_apoli.NeoApoli;
+
 import java.util.Iterator;
 import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
-public class Services {
+public final class Services {
 
 	public static <T> T load(Class<T> serviceClass) {
 
 		ServiceLoader<T> loader = ServiceLoader.load(serviceClass, Services.class.getClassLoader());
 		Iterator<T> iterator = loader.iterator();
 
-		RuntimeException exception = new IllegalStateException("Couldn't load service for " + serviceClass.getName());
 		while (iterator.hasNext()) {
 
 			try {
@@ -19,12 +20,12 @@ public class Services {
 			}
 
 			catch (ServiceConfigurationError error) {
-				exception.addSuppressed(error);
+				NeoApoli.LOGGER.debug(error.getMessage(), error);
 			}
 
 		}
 
-		throw exception;
+		throw new IllegalStateException("Couldn't load service for " + serviceClass.getName());
 
 	}
 
