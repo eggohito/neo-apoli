e45b27bde0830ffb4b6a64270fa9d63132a78dc8
eggohito <66972816+eggohito@users.noreply.github.com>
2026-08-04 15:37:15 +0800

    Added `biome/foliage` color type

--- src/main/java/io/github/eggohito/neo_apoli/registry/NeoApoliColorResolvers.java
+++ src/main/java/io/github/eggohito/neo_apoli/registry/NeoApoliColorResolvers.java
@@ -1,15 +1,22 @@
 package io.github.eggohito.neo_apoli.registry;
 
 import net.minecraft.world.level.ColorResolver;
+import net.minecraft.world.level.biome.Biome;
 
 import java.util.function.Consumer;
 
+//  Biome color resolvers from vanilla copied to be physical-side agnostic
 public final class NeoApoliColorResolvers {
 
-	//  A physical side-agnostic color resolver for getting the biome's water color
+	public static final ColorResolver BIOME_DRY_FOLIAGE_COLOR  = (biome, x, y) -> biome.getDryFoliageColor();
+	public static final ColorResolver BIOME_FOLIAGE_COLOR = (biome, x, y) -> biome.getFoliageColor();
+	public static final ColorResolver BIOME_GRASS_COLOR = Biome::getGrassColor;
 	public static final ColorResolver BIOME_WATER_COLOR = (biome, x, y) -> biome.getWaterColor();
 
 	public static void registerAll(Consumer<ColorResolver> registrant) {
+		registrant.accept(BIOME_DRY_FOLIAGE_COLOR);
+		registrant.accept(BIOME_FOLIAGE_COLOR);
+		registrant.accept(BIOME_GRASS_COLOR);
 		registrant.accept(BIOME_WATER_COLOR);
 	}
 
