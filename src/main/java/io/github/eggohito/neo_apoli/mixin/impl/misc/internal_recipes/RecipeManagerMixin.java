9557eb40babaad41ad8bb5df4b5ab59f045c5a9e
eggohito <66972816+eggohito@users.noreply.github.com>
2026-09-02 19:17:52 +0800

    Tidied up mixin for filtering internal recipes

--- src/main/java/io/github/eggohito/neo_apoli/mixin/impl/misc/internal_recipes/RecipeManagerMixin.java
+++ src/main/java/io/github/eggohito/neo_apoli/mixin/impl/misc/internal_recipes/RecipeManagerMixin.java
@@ -2,14 +2,13 @@ package io.github.eggohito.neo_apoli.mixin.impl.misc.internal_recipes;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import io.github.eggohito.neo_apoli.NeoApoli;
 import io.github.eggohito.neo_apoli.util.RecipeUtil;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.crafting.Recipe;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeManager;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.item.crafting.RecipeMap;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
@@ -17,24 +16,18 @@ import java.util.List;
 @Mixin(RecipeManager.class)
 public abstract class RecipeManagerMixin {
 
-	@Shadow
-	@Final
-	private static Logger LOGGER;
+	@WrapOperation(method = "prepare(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)Lnet/minecraft/world/item/crafting/RecipeMap;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/crafting/RecipeMap;create(Ljava/lang/Iterable;)Lnet/minecraft/world/item/crafting/RecipeMap;"))
+	RecipeMap filterInternalRecipeTypes(Iterable<RecipeHolder<?>> recipes, Operation<RecipeMap> original) {
 
-	@WrapOperation(method = "method_64689", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
-	private static <E> boolean filterInternalRecipeTypes(List<E> list, E element, Operation<Boolean> original, List<E> mList, ResourceLocation mId, Recipe<?> mRecipe) {
+		List<RecipeHolder<?>> filtered = new ObjectArrayList<>();
 
-		boolean result = RecipeUtil.validateRecipe(mRecipe)
-			.ifError(error -> LOGGER.error("Couldn't register recipe \"{}\": {}", mId, error.message()))
-			.isSuccess();
-
-		if (result) {
-			return original.call(list, element);
+		for (var recipe : recipes) {
+			RecipeUtil.validateRecipe(recipe.value())
+				.ifSuccess(ignored -> filtered.add(recipe))
+				.ifError(error -> NeoApoli.LOGGER.error("Couldn't register recipe \"{}\": {}", recipe.id(), error.message()));
 		}
 
-		else {
-			return false;
-		}
+		return original.call(filtered);
 
 	}
 
